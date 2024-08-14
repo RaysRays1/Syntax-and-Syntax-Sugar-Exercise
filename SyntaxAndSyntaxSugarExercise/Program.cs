35173240c0242bfc498c64d2702842930dd2fa5c
Raynard Brown <brown_raynard@icloud.com>
2024-08-14 15:15:16 -0500

    Exercise1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,38 @@
     public class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            // Given the following code, change the code to implement:
+
+            int answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " greater than or equal to nine";
+            }
+
+           // Console.WriteLine(response);
+            
+            // String Interpolation:
+            // Ternary Operation:
+            // Conditions ?, Truescope, Falsescope
+
+
+            
+            response = (answer < 9) ? "Is less than nine!" : "Greater than or equal to nine";
+            Console.WriteLine(response);
+
+
+
+
         }
+        
+        
+        
     }
 }
